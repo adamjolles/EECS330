5780052980a5d6143b01d70969950230a5530a5b
Adam Jolles <adamjolles8@gmail.com>
2023-03-06 12:50:43 -0600

    Practice

--- practice/data-structures/linkedList.cpp
+++ practice/data-structures/linkedList.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
 #include <string>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
-class node {
-    public:
-        int data;
-        node* next;
-
-        node(int val) {
-            data = val;
-            next = NULL;
-        }
+struct node {
+    int data;
+    node *next;
 };
+
+// int main() {
+//     node *head = NULL;
+//     node *temp = NULL;
+
+//     temp = new node;
+//     temp->data = 2;
+//     temp->next = NULL;
+//     head = temp;
+
+//     cout << temp << endl;
+//     delete temp;
+// }
+
+
+int main() {
+    int a = 5;
+    int& b = a;
+    cout << b << endl;
+    cout << &b << endl;
+    cout << &a << endl;
+}
